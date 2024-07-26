080784f4a69dbb92254f627ad131d2083aab4610
Raymond Luo <rrl290@nyu.edu>
2024-07-26 00:33:00 -0400

    [ALPHA-001] refactor newton method. review: Self

--- math_library/Solver/NewtonSolver.hpp
+++ math_library/Solver/NewtonSolver.hpp
@@ -1,37 +1,51 @@
 // C++ program for implementation of Newton Raphson Method for
 // solving equations
 #include<bits/stdc++.h>
+#include<functional>
 using namespace std;
 
 
 namespace SolverLib {
-    // An example function whose solution is determined using
-    // Bisection Method. The function is x^3 - x^2  + 2
-    double func(double x)
-    {
-        return x * x * x - x * x + 2;
-    }
 
-    // Derivative of the above function which is 3*x^x - 2*x
-    double derivFunc(double x)
-    {
-        return 3 * x * x - 2 * x;
-    }
+    class Solver {
+        function<double(double)> func;
+        function<double(double)> grad;
+        Solver(double (*_func)(double), double (*_grad)(double)) {
+            func = _func;
+            grad = _grad;
+        }
+    };
 
-    // Function to find the root
-    double newtonRaphson(double x, double EPSILON = 0.0001)
-    {
-        double h = func(x) / derivFunc(x);
-        while (abs(h) >= EPSILON)
+    class NewtonMethod :Solver {
+        // An example function whose solution is determined using
+        // Bisection Method. The function is x^3 - x^2  + 2
+        double func(double x)
         {
-            h = func(x) / derivFunc(x);
-
-            // x(i+1) = x(i) - f(x) / f'(x)  
-            x = x - h;
+            return x * x * x - x * x + 2;
         }
 
-        return x;
-    }
+        // Derivative of the above function which is 3*x^x - 2*x
+        double derivFunc(double x)
+        {
+            return 3 * x * x - 2 * x;
+        }
+
+        // Function to find the root
+        double newtonRaphson(double x, double EPSILON = 0.0001)
+        {
+            double h = func(x) / derivFunc(x);
+            while (abs(h) >= EPSILON)
+            {
+                h = func(x) / derivFunc(x);
+
+                // x(i+1) = x(i) - f(x) / f'(x)  
+                x = x - h;
+            }
+
+            return x;
+        }
+
+    };
 
     // Driver program to test above
     int main()
